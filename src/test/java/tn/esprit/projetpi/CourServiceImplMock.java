457b17bc621aa8b11e1eab5088a425424051ac38
chaimaBG1 <114779712+chaimaBG1@users.noreply.github.com>
2024-10-31 16:43:11 +0100

    Update CourServiceImplMock.java

--- src/test/java/tn/esprit/projetpi/CourServiceImplMock.java
+++ src/test/java/tn/esprit/projetpi/CourServiceImplMock.java
@@ -17,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-class CoursServiceTest {
+class CourServiceImplMock {
 
     @InjectMocks
     private CoursService coursService;
